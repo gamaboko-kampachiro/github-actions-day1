aea57f8bc5d32aaa3c74b86ac0fc8f9987d21dab
Manish <manish@email.com>
2025-07-23 23:21:15 +0530

    day 3

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 def multiply(a, b):
     return a * b
 
+
 if __name__ == "__main__":
     print("2 x 3 =", multiply(2, 3))
--- test_app.py
+++ test_app.py
@@ -1,5 +1,6 @@
 from app import multiply
 
+
 def test_multiply():
     assert multiply(2, 3) == 6
     assert multiply(0, 5) == 0
